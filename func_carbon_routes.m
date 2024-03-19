04be25c8e95e1a365c9d4253cad03c788253416f
maddysshaw <99265137+maddysshaw@users.noreply.github.com>
2024-03-19 10:47:37 +0000

    Update func_carbon_routes.m adding values

--- func_carbon_routes.m
+++ func_carbon_routes.m
@@ -1,4 +1,10 @@
-function [out] = func_carbon_routes(distance, count, rate, takeoff, landing)
+function [out] = func_carbon_routes(distance, count)
 %Calculates carbon per route
-out = count*(takeoff + landing + (distance*rate));
+taxi_out = 450;
+takeoff = 150;
+climb = 400;
+landing = 300;
+taxi_in = 200;
+rate = 17.05; %carbon emissions per nmi
+out = count*(taxi_out + takeoff + climb + landing + taxi_in + (distance*rate));
 end
